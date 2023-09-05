f730d89642c12045d647b920b25347aa7e2d00e7
luciamoyanotello <luciaamancay@gmail.com>
2023-09-05 20:13:41 -0300

    HTML a JSX

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import './App.css';
 
 function App() {
   return (
-    <>
-  <h1>Tareas Pendientes de Hedy Lamarr</h1>
-  <img
-    src="https://i.imgur.com/yXOvdOSs.jpg"
-    alt="Hedy Lamarr"
-    className="photo"
-  />
-  <ul>
-    <li>Inventar nuevo semáforo</li>
-    <li>Ensayar la escena de la película</li>
-    <li>Mejorar la tecnología del espectro</li>
-  </ul>
-</>
+    <div className="App">
+      <header className="App-header">
+      <>
+        <h1>Tareas Pendientes de Hedy Lamarr</h1>
+        <img
+          src="https://i.imgur.com/yXOvdOSs.jpg"
+          alt="Hedy Lamarr"
+          className="photo"
+        />
+        <ul>
+          <li>Inventar nuevo semáforo</li>
+          <li>Ensayar la escena de la película</li>
+          <li>Mejorar la tecnología del espectro</li>
+        </ul>
+      </>
+      </header>
+    </div>
   );
 }
 
